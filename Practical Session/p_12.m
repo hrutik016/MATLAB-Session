10ac4b1a081ac1d88fb66550cc6b5d0bd763a998
hrutik016 <hrutik716@gmail.com>
2021-08-06 11:50:48 +0530

    addded p_12

--- Practical Session/p_12.m
+++ Practical Session/p_12.m
@@ -1,6 +1,28 @@
-% slicing
+% threshold slicing 
 
 clc;
 clear all;
 close all;
 
+img = imread('D:\hrutik\Wallpapers\DIP images\1.jpg');
+
+y = double(img);
+
+[m, n] = size(y);
+
+a = input('lower threshold value : ');
+b = input('upper threshold value : ');
+
+for i=1:m
+    for j=1:n
+        if (y(i, j) >= a & y(i, j) <= b)
+            z(i, j) = 255;
+        else
+            z(i, j) = y(i, j);
+        end
+    end
+end
+
+figure, imshow(uint8(z));
+%subplot(121); imshow(img); title('original image');
+%subplot(122); imshow(z); title('resultant image');
